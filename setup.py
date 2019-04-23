1f03a6216e1f5ccf1506846fbeab0dd666e227ee
nicholasdeporzio <ndeporzio@gmail.com>
2019-04-22 21:32:06 -0400

    Setup python packaging minimal structure.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cosmicfish',
+      version='0.1.0',
+      description='A Fisher forecasting package for light relic cosmology.',
+      url='http://github.com/ndeporzio/cosmicfish',
+      author='Nicholas DePorzio, Julian Munoz',
+      author_email='nicholasdeporzio@g.harvard.edu',
+      license='',
+      packages=['cosmicfish'],
+      zip_safe=False)
