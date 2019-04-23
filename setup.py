5d5a5ec5a8812f3f0878ed4c8a319c90caa0a3f7
nicholasdeporzio <ndeporzio@gmail.com>
2019-04-22 21:45:41 -0400

    Added package dependencies, information.

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from setuptools import setup
 setup(name='cosmicfish',
       version='0.1.0',
       description='A Fisher forecasting package for light relic cosmology.',
+      keywords=['fisher forecast cosmology neutrino dark matter']
       url='http://github.com/ndeporzio/cosmicfish',
       author='Nicholas DePorzio, Julian Munoz',
       author_email='nicholasdeporzio@g.harvard.edu',
       license='',
       packages=['cosmicfish'],
-      zip_safe=False)
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'numpy',
+          'matplotlib', 
+      ])
