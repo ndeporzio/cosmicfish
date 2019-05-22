4abfbe7a7d85bdce0cb4b8567937d443f6b8ecd0
nicholasdeporzio <ndeporzio@gmail.com>
2019-05-22 01:13:06 -0400

    Resolved class compilation problem.

--- bin/install_class.sh
+++ bin/install_class.sh
@@ -4,10 +4,18 @@
 set -o errexit 
 
 #Collect filepath to install CLASS at
+echo "Your version of gcc is: "
+gcc --version
+echo "Look up!"
+echo "Is your gcc version at least 4.9? If not, edit the 'CC = ' line"
+echo "in the MakeFile to point to such a version. Cancel execution"
+echo "then re-run."
+
 echo "Enter absolute path to project directory..."
 read -p "Project Path: " path
 echo "Installing CLASS at the following location: "
 echo "$path/lib/"
+mkdir "$path/lib/"
 cd $path/lib/
 
 #Install CLASS
@@ -15,7 +23,7 @@ git clone git@github.com:lesgourg/class_public.git
 mv class_public class
 cd class 
 make clean
-make class
+make -j class
 
 #Distribute path to CLASS
 CLASS_DIR=$PWD
