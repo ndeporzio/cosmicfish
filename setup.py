12cf5485b2e0ecc0f9195990f31670354d2fa178
Nicholas DePorzio <deporzio.n@husky.neu.edu>
2019-08-12 14:08:15 -0400

    Change project version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cosmicfish',
-      version='0.1.0',
+      version='0.2.0',
       description='A Fisher forecasting package for light relic cosmology.',
       keywords=['fisher forecast cosmology neutrino dark matter'], 
       url='https://github.com/ndeporzio/cosmicfish',
