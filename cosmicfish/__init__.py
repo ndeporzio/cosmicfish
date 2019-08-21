9e37645851997380d498b7807c6c9eb32e8fbc80
Nicholas DePorzio <deporzio.n@husky.neu.edu>
2019-08-20 23:42:45 -0400

    Restructure convergence class.

--- cosmicfish/__init__.py
+++ cosmicfish/__init__.py
@@ -1,12 +1,11 @@
-from .constants import (C, FULL_SKY_DEGREES, NEUTRINO_SCALE_FACTOR, SIGMA_Z,
-    KFS_NUMERATOR_FACTOR, KFS_DENOMINATOR_FACTOR, RSD_GAMMA,
+from .constants import (CLASSVARS, C, FULL_SKY_DEGREES, NEUTRINO_SCALE_FACTOR, 
+    SIGMA_Z, KFS_NUMERATOR_FACTOR, KFS_DENOMINATOR_FACTOR, RSD_GAMMA,
     RSD_DELTA_Q, RSD_Q_NUMERATOR_FACTOR, RSD_DELTA_L_NUMERATOR_FACTOR,
-    RELIC_TEMP_SCALE, KP_PREFACTOR, FID_SIGMA_FOG_0, FID_B1L, FID_ALPHAK2,
-    DB_ELG, K_MAX, K_MIN)
+    RELIC_TEMP_SCALE, KP_PREFACTOR, DB_ELG, K_MAX, K_MIN)
 from .equations import (H, Da, neff, fog, log_fog, kfs, rsd, log_rsd, 
     omega_ncdm, T_ncdm, domega_ncdm_dT_ncdm, dT_ncdm_domega_ncdm, sigmafog, 
     sigmav, fgrowth, ggrowth, btildebias, gen_V, gen_k_table, ap, log_ap,
-    cov, cov_dkdH, cov_dkdDa) 
+    cov, cov_dkdH, cov_dkdDa, set_sky_cover) 
 from .methods import (dPs_array, dPs, dlogPs, derivative) 
 from .convergence import (convergence)
 from .forecast import (forecast) 
