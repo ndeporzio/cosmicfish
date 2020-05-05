57821f80945a420c8b11359cdaac1425be64fd35
Nicholas DePorzio <deporzio.n@husky.neu.edu>
2020-05-05 16:00:30 -0400

    Corrected growth factor column number in class output.

--- cosmicfish/data.py
+++ cosmicfish/data.py
@@ -134,7 +134,7 @@ class spectrum:
         if self.forecast=="neutrino":
             colidx = 20
         elif self.forecast=="relic": 
-            colidx = 19                                                 
+            colidx = 16                                                
         rawdata = pd.read_csv(self.background_data,                             
                               delim_whitespace=True,                            
                               skipinitialspace=True,                            
