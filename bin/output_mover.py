14889fb9649169e49c95fc60dddceee213d28970
Nicholas DePorzio <nicholasdeporzio@g.harvard.edu>
2020-09-23 03:32:21 -0400

    Improved output_mover.py script.

--- bin/output_mover.py
+++ bin/output_mover.py
@@ -1,15 +1,48 @@
 import os
 import shutil
 
-prepath_in='/n/home02/ndeporzio/projects/cosmicfish/cfworkspace/results/FINAL_RESULTS/EUCLID/EUCLID_GridPlot_'
-prepath_out='/n/home02/ndeporzio/projects/cosmicfish/cfworkspace/results/FINAL_RESULTS/Parsed/EUCLID/EUCLID_GridPlot_'
+lss_survey = 'BOSS' # 'BOSS' or 'DESI' or 'EUCLID' 
+plot_type = 'Fine' # 'Fine' or 'Grid' 
+results_dir_name = 'FINAL_RESULTS_18SEP2020'
 
-for idx in range(120): 
-    tidx=idx//10
-    midx=idx%10
-    path=prepath_out+str(idx)+'/'
-    os.mkdir(path)
-    filename= ('gp_'+str(tidx)+'_'+str(midx)+'.db')
+###################################
+##  USER ONLY MODIFY ABOVE HERE  ##
+###################################
+
+prepath_in= ('/n/home02/ndeporzio/projects/cosmicfish/cfworkspace/results/'
+    + results_dir_name
+    + '/'
+    + lss_survey
+    + '/'
+    + lss_survey
+    + '_'
+    + plot_type
+    + 'Plot_') 
+
+prepath_out = ('/n/home02/ndeporzio/projects/cosmicfish/cfworkspace/results/'
+    + results_dir_name 
+    + '/Parsed/'
+    + lss_survey
+    + '/'
+    + lss_survey
+    + '_'
+    + plot_type
+    + 'Plot_')
+
+if plot_type == 'Grid': 
+    max_idx = 120
+    mass_idx = 10
+    prefix = 'gp'
+elif plot_type == 'Fine': 
+    max_idx = 84
+    mass_idx = 21
+    prefix = 'fp'  
+
+for idx in range(max_idx): 
+    tidx=idx//mass_idx
+    midx=idx%mass_idx
+    path=(prepath_out + str(idx) + '/')
+    os.makedirs(path, exist_ok=True)
+    filename= (prefix + '_' + str(tidx) + '_' + str(midx)+'.db')
     shutil.copyfile(prepath_in+str(idx)+'/'+filename, prepath_out+str(idx)+'/'+filename)
     print(path) 
-#    os.rename(prepath_in+str(idx)+'.err', prepath_out+str(idx)+'/EUCLID_GridPlot_56234229_'+str(idx)+'.err')
